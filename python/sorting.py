cc943f43b9bdfbcdb4aacca48de1210ebb0bbd28
Catman6094 <fiercekitty131@gmail.com>
2023-02-17 09:28:04 -0500

    more merge sort

--- python/sorting.py
+++ python/sorting.py
@@ -19,19 +19,19 @@ def insertion_sort(A):
 
 
 # MERGE SORT
-def merge(A, left, mid, right):
-    # Merge A[left:mid] and A[mid:right]
+def _merge(A, left, mid, right):
+    # Merge operation for merge sort
     
     # Create arrays with infinite sentinel values
-    L = A[left:mid]
-    R = A[mid:right]
+    L = A[left:mid+1]
+    R = A[mid+1:right+1]
     L.append(_inf())
     R.append(_inf())
     
     li = 0
     ri = 0
         
-    for i in range(left, right):
+    for i in range(left, right + 1):
         if L[li] > R[ri]:
             A[i] = R[ri]
             ri += 1
@@ -41,13 +41,17 @@ def merge(A, left, mid, right):
 
     
  
-def merge_sort(A, left, right):
-    # Sorts in [left:right]
-    if right - left > 1:
+def merge_sort(A, left=None, right=None):
+    # Sorts in [left:right+1]
+    
+    if left is None: left = 0
+    if right is None: right = len(A) - 1
+    
+    if right - left > 0:
         mid = (left + right) // 2
         merge_sort(A, left, mid)
-        merge_sort(A, mid, right)
-        merge(A, left, mid, right)
+        merge_sort(A, mid+1, right)
+        _merge(A, left, mid, right)
 
 
 if __name__ == "__main__":
@@ -56,5 +60,9 @@ if __name__ == "__main__":
     assert ns == [1, 2, 3, 4, 5, 6]
     
     ns = [3, 2, 6, 5, 1, 4]
-    merge_sort(ns, 0, len(ns))
+    merge_sort(ns)
     assert ns == [1, 2, 3, 4, 5, 6]
+    
+    ns = [3, 2, 6, 5, 1, 4]
+    merge_sort(ns, 1, 4)
+    assert ns == [3, 1, 2, 5, 6, 4]
